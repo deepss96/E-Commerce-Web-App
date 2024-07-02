7fd85c82920c019ff8d39e9c96cfe86b23f82567
deepss96 <111363738+deepss96@users.noreply.github.com>
2024-07-02 15:51:30 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,13 @@ import userRoute from "./route/user.route.js"
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: [""],
+    methods: ["POST", "GET"],
+    Credentials: true
+  }
+));
 app.use(express.json());
 
 dotenv.config();
